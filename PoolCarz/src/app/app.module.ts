eb08b21417064248ab89b2f0bf98b7507cbd70ec
Priyanka <Priyanka_bs@hotmail.com>
2018-04-08 15:31:52 -0400

    Add the chages to PoolcarAPP

--- PoolCarz/src/app/app.module.ts
+++ PoolCarz/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
